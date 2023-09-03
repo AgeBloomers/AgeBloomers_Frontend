90a1e9460c7a5b0a93147ba72efcb79496139aab
TofuMango <yjlee0006@cu.ac.kr>
2023-09-03 18:31:18 +0900

    add&style: FindSitter_All 컴포넌트 내부렌더링을 위한 FindMore컴포넌트 추가 및 style적용

--- src/components/Detail/FindSitter_All.jsx
+++ src/components/Detail/FindSitter_All.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
+import FindMore from "./FindMore";
 
 const FindSitter_All = () => {
+  // 선택된 카드의 ID를 추적하는 상태 변수
+  const [selectedCard, setSelectedCard] = useState(null);
+
   // 카드 데이터 배열
-  const cardData = [
+  const cards = [
     {
       id: 1,
       title: "이름 : 김수정",
@@ -29,65 +33,83 @@ const FindSitter_All = () => {
     },
     {
       id: 5,
+      title: "이름 : 두부",
+      area: "지역 : 대구",
+      type: "유형 : 베이비시터",
+    },
+    {
+      id: 6,
       title: "이름 : 루스",
       area: "지역 : 경북",
       type: "유형 : 부모",
     },
-    {
-      id: 6,
-      title: "이름 : 두부",
-      area: "지역 : 경북",
-      type: "유형 : 베이비시터",
-    },
-    {
-      id: 7,
-      title: "이름 : 콜라",
-      area: "지역 : 경북",
-      type: "유형 : 베이비시터",
-    },
-    // 더미임
+    // 다른 카드 데이터도 여기에 추가 가능
   ];
+
+  // 카드를 클릭했을 때 실행되는 함수
+  const handleCardClick = (id) => {
+    setSelectedCard(id); // 선택된 카드의 ID를 저장하여 FindMore를 렌더링하게 함
+  };
+
+  // FindMore 컴포넌트를 닫는 함수
+  const handleExpandClose = () => {
+    setSelectedCard(null); // 선택된 카드의 ID를 초기화하여 FindMore를 닫음
+  };
 
   // 카드 개수에 따라 divStyle의 height 계산
   const [divStyle, setDivStyle] = useState({
     width: "600px",
-    height: `${cardData.length * 150}px`, // 카드 하나당 약 150px 높이로 가정
+    height: `${cards.length * 200}px`, // 카드 하나당 약 200px 높이로 가정
   });
 
   useEffect(() => {
     setDivStyle((prevStyle) => ({
       ...prevStyle,
-      height: `${cardData.length * 150}px`, // 카드 개수에 따라 높이 업데이트
+      height: `${cards.length * 200}px`, // 카드 개수에 따라 높이 업데이트
     }));
-  }, [cardData.length]);
+  }, [cards.length]);
+
+  const findMoreStyle = {
+    marginTop: "-300px", // 원하는 여백 크기로 조절
+    marginBottom:"-300px"
+  };
 
   return (
     <div>
       <div className="flex justify-center items-center min-h-screen">
         <div className="bg-white rounded-3xl" style={divStyle}>
-          {/* 카드 생성을 반복문으로 */}
-          {cardData.map((card) => (
-            <div key={card.id} className="p-4">
-              <div className="bg-gray-100 rounded-3xl p-4 flex items-center">
-                <div>
-                  <h2 className="text-lg font-Pretendard font-semibold text-left">
-                    {card.title}
-                  </h2>
-                  <h2 className="text-lg font-Pretendard font-semibold text-left">
-                    {card.area}
-                  </h2>
-                  <h2 className="text-lg font-Pretendard font-semibold text-left">
-                    {card.type}
-                  </h2>
-                  <p className="text-gray-500">{card.content}</p>
+          {cards.map((card) => (
+            <div key={card.id} className="p-2">
+              {/* 선택된 카드와 현재 카드의 ID가 일치하면 FindMore를 렌더링, 그렇지 않으면 일반 카드를 렌더링 */}
+              {selectedCard === card.id ? (
+                <div style={findMoreStyle}>
+                <FindMore onClose={handleExpandClose} selectedCard={selectedCard} />
                 </div>
-                <button className="bg-525151 text-FFD700 w-24 h-10 rounded-full hover:bg-00473E font-Pretendard ml-auto">
-                  상세
-                </button>
-              </div>
+              ) : (
+                <div className="bg-gray-100 rounded-3xl p-4 flex items-center">
+                  <div>
+                    <h2 className="text-lg font-Pretendard font-semibold text-left">
+                      {card.title}
+                    </h2>
+                    <h2 className="text-lg font-Pretendard font-semibold text-left">
+                      {card.area}
+                    </h2>
+                    <h2 className="text-lg font-Pretendard font-semibold text-left">
+                      {card.type}
+                    </h2>
+                    <p className="text-gray-500">{card.content}</p>
+                  </div>
+                  {/* 카드를 클릭하면 해당 카드의 ID를 setSelectedCard로 전달 */}
+                  <button
+                    onClick={() => handleCardClick(card.id)}
+                    className="bg-525151 text-FFD700 w-24 h-10 rounded-full hover:bg-00473E font-Pretendard ml-auto"
+                  >
+                    상세
+                  </button>
+                </div>
+              )}
             </div>
           ))}
-          {/* 카드 생성 반복문 끝 */}
         </div>
       </div>
     </div>
